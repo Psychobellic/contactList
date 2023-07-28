24307684ec6142062b390af64f4b1e6de897addd
Matheus Schiffer <matheusschiffer@gmail.com>
2023-07-28 13:20:04 -0300

    bootstrap on new resource views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  # CRUD Resource Routes
   resources :contacts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/about", to: 'home#about'
-
   # Defines the root path route ("/")
-  root 'home#index'
+  root 'contacts#index'
 end
